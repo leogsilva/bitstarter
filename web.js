3e810f6bee4966b3c0eab2e6340fe362dc3c9e00
Leonardo Silva <leogsilva@gmail.com>
2013-07-07 14:59:21 +0000

    serving 2

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
-var express = require('express');
+
+var express = require('express'),
+	fs = require('fs')
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/index.html', function(request, response) {
+  fs.readFile('./index.html',function(err, html) {
+    if (err)
+	throw err;
+    response.send(html);
+  }); 
 });
 
 var port = process.env.PORT || 5000;
